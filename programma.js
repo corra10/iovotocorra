8219ca774854831f9d8861b8469729ba1b932ba9
Matilde Galati <matilde.galati>
2024-11-13 13:38:22 +0100

    disegno

---
+++ programma.js
@@ -0,0 +1,17 @@
+
+
+let foglio = document.getElementById('foglio');
+
+let disegno = foglio.getContext('2d');
+
+disegno.fillStyle = 'red'
+
+disegno.fillRect(180,90, 100, 200)
+
+disegno.fillStyle = 'green'
+
+disegno.fillRect(250,250, 200, 100)
+
+disegno.fillStyle = 'yellow'
+
+disegno.fillRect(220,280, 80, 200)

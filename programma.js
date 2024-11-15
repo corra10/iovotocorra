6fc83ff4c688de72cdf699f2be7a119a3fefe67d
corra10 <matilde.galati@iismontalegenova.it>
2024-11-15 10:36:16 +0100

    pubblicazione

--- programma.js
+++ programma.js
@@ -6,12 +6,40 @@ let disegno = foglio.getContext('2d');
 
 disegno.fillStyle = 'red'
 
-disegno.fillRect(180,90, 100, 200)
+disegno.fillRect(150,0, 400, 350)
 
-disegno.fillStyle = 'green'
+disegno.fillStyle = 'black'
 
-disegno.fillRect(250,250, 200, 100)
+disegno.fillRect(0,350, 500, 10 )
+
+disegno.fillStyle = 'black'
+
+disegno.fillRect(150,0, 10, 500)
+
+disegno.fillStyle = 'blue'
+
+disegno.fillRect(0,360, 150, 150)
+
+disegno.fillStyle = 'black'
+
+disegno.fillRect(0,150, 150, 20)
+
+disegno.fillStyle = 'black'
+
+disegno.fillRect(440,360, 10, 150)
+
+disegno.fillStyle = 'black'
+
+disegno.fillRect(450,420, 50, 20)
 
 disegno.fillStyle = 'yellow'
 
-disegno.fillRect(220,280, 80, 200)
+
+
+let corra = document.getElementById('corra');
+
+let disegno2 = corra.getContext('2d');
+
+corra.addEventListener('click', (evento)=> {
+    disegno2.strokeRect( evento.offsetX, evento.offsetY, 500*Math.random(), 500*Math.random());
+});
